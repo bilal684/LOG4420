289bf1ba28c02c3ce2e3d674dc1b1337c327d4f1
Bilal <bilal.itani1234@gmail.com>
2018-10-26 19:37:43 -0400

    Added confirmation page javascript.

--- TP3/assets/scripts/confirmation.js
+++ TP3/assets/scripts/confirmation.js
@@ -1,3 +1,9 @@
 $(function(){
     utils.updateShoppingCartBadge()
+
+    let acc = JSON.parse(localStorage.getItem("compte"))
+    $("#name").text(acc.prenom + " " + acc.nom)
+
+    let confirmationNumber = localStorage.getItem("confirmation")
+    $("#confirmation-number").text(confirmationNumber)
 })
