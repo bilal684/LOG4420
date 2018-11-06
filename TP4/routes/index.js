cfec37c2cd3d6924dffea374d3ab054332e7c046
Said Hmani <said.hmani@gmail.ca>
2018-11-06 16:07:48 -0500

    coder Index.js

--- TP4/routes/index.js
+++ TP4/routes/index.js
@@ -2,7 +2,35 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { title: "Accueil", message: "Ça semble fonctionner!" });
+  res.render("index", { title: "OnlineShop - Accueil" });
+});
+
+router.get("/accueil", (req, res) => {
+  res.render("index", { title: "OnlineShop - Accueil" });
+});
+
+router.get("/produits", (req, res) => {
+  res.render("produits", { title: "OnlineShop - Produits" });
+});
+
+router.get("/produit", (req, res) => {
+  res.render("produit", { title: "OnlineShop - Produit" });
+});
+
+router.get("/contact", (req, res) => {
+  res.render("contact", { title: "OnlineShop - Contact" });
+});
+
+router.get("/panier", (req, res) => {
+  res.render("panier", { title: "OnlineShop - Panier" });
+});
+
+router.get("/commande", (req, res) => {
+  res.render("commande", { title: "OnlineShop - Commande" });
+});
+
+router.get("/confirmation", (req, res) => {
+  res.render("confirmation", { title: "OnlineShop - Confirmation" });
 });
 
 module.exports = router;
