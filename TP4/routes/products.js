84fb8d3b7b8703251e9d2cc6d0045250adc4637f
Bilal <bilal.itani1234@gmail.com>
2018-11-10 16:37:50 -0500

    Fixed products.js

--- TP4/routes/products.js
+++ TP4/routes/products.js
@@ -13,31 +13,27 @@ router.get("/", function(req, res)
         let criteria = req.query.criteria
         if(category && criteria)
         {
-            if ( category !== "cameras" && category !== "computers" && category !== "consoles" && category !== "screens" &&
-            criteria !== "alpha-asc" && criteria !== "alpha-dsc" && criteria !== "price-asc" && criteria !== "price-dsc")
-			{
-				return res.status(400).send("Invalid category or criteria.")
-			}
-			else
-			{
-				Product.find({"category" : category}, function(err,products)
+            if((category === "cameras" || category === "computers" || category === "consoles" || category === "screens" || category === "all") &&
+                (criteria === "alpha-asc" || criteria === "alpha-dsc" || criteria === "price-asc" || criteria === "price-dsc"))
+            {
+                Product.find({"category" : category}, function(err,products)
 				{  
                     utilities.sortProducts(criteria, products)
 					res.status(200)
 			        res.json(products)
 					res.end()
 			    });
+            }
+			else
+			{
+                return res.status(400).send("Invalid category or criteria.")
 			}
         }
         else if(category)
         {
-            if ( category !== "cameras" && category !== "computers" && category !== "consoles" && category !== "screens") 
+            if (category === "cameras" || category === "computers" || category === "consoles" || category === "screens") 
 			{
-				return res.status(400).send("Invalid category.")
-			}
-			else
-			{
-				Product.find({"category" : category}, function(err,products) 
+                Product.find({"category" : category}, function(err,products) 
 				{ 
                     utilities.sortProducts("price-asc", products)
 					res.status(200)
@@ -45,22 +41,26 @@ router.get("/", function(req, res)
 					res.end()
 			    });
 			}
+			else
+			{
+                return res.status(400).send("Invalid category.")
+			}
         }
         else if (criteria)
         {
-            if (criteria !== "alpha-asc" && criteria !== "alpha-dsc" && criteria !== "price-asc" && criteria !== "price-dsc")
+            if (criteria === "alpha-asc" || criteria === "alpha-dsc" || criteria === "price-asc" || criteria === "price-dsc")
 			{
-				return res.status(400).send("Invalid criteria.")
-			}
-			else
-			{
-				Product.find({"category" : category}, function(err,products)
+                Product.find({}, function(err,products)
 				{  
                     utilities.sortProducts(criteria, products)
 					res.status(200)
 			        res.json(products)
 					res.end()
 			    });
+			}
+			else
+			{
+                return res.status(400).send("Invalid criteria.")
 			}
         }
         else
